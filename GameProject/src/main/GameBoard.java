08fe2169282e0eaaccf2d4637bef00c1e7632c0f
Yasin <95018429+yasinfahmy@users.noreply.github.com>
2021-12-19 16:04:31 +0100

    Wow

---
+++ GameProject/src/main/GameBoard.java
@@ -0,0 +1,9 @@
+package main;
+
+import java.util.ArrayList;
+
+public class GameBoard {
+    char[][] grid;
+    ArrayList<Character> tiles;
+    public enum PremiumSquare {DOUBLE_LETTER_SCORE,TRIPLE_LETTER_SCORE, DOUBLE_WORD_SCORE, TRIPLE_WORD_SCORE}
+}
---
+++ GameProject/src/main/Player.java
@@ -0,0 +1,8 @@
+package main;
+
+public class Player {
+    private String name;
+    private int score;
+    private char[] rack;
+
+}
