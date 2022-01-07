0a53ac92a299d9c966e84a91c27f22a2aeb7d851
Maxim020 <m.frano@student.utwente.nl>
2022-01-07 12:11:26 +0100

    7.1. 2022 12:10 CET
    - Changed Player class to an interface and created two new classes for
    Human player and Computer player

---
+++ GameProject/src/model/ComputerPlayer.java
@@ -0,0 +1,35 @@
+package model;
+
+public class ComputerPlayer implements Player {
+	
+	private String name;
+	private int score;
+	private LetterDeck letterdeck;
+	
+	public ComputerPlayer(String name, Bag bag) {
+		this.name = name;
+		this.letterdeck = new LetterDeck(bag);
+		this.score = 0;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public LetterDeck getLetterDeck() {
+		return letterdeck;
+	}
+
+	public int getScore() {
+		return score;
+	}
+
+	public void setScore(int score) {
+		this.score = score;
+	}
+
+}
--- GameProject/src/model/Player.java
+++ GameProject/src/model/Player.java
@@ -1,37 +1,15 @@
 package model;
 
-public class Player {
+public interface Player {
 	
-	//Attributes
-    private String name;
-    private int score;
-    private char[] rack;
-    
-    //Constructor
-    public Player(String name) {
-    	this.name = name;
-    }
-    
-    //Methods
-	public String getName() {
-		return name;
-	}
-
-	public int getScore() {
-		return score;
-	}
-
-	public void setScore(int score) {
-		this.score = score;
-	}
-
-	public char[] getRack() {
-		return rack;
-	}
-
-	public void setRack(char[] rack) {
-		this.rack = rack;
-	}
-    
+	public String getName();
+	
+	public void setName(String name);
+	
+	public LetterDeck getLetterDeck();
+	
+	public int getScore();
+	
+	public void setScore(int score);
 
 }
