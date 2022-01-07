33201b46f20bb38f1c80f3471763373dc387d17a
Maxim020 <m.frano@student.utwente.nl>
2022-01-07 13:59:02 +0100

    Update GameBoard.java

--- GameProject/src/model/GameBoard.java
+++ GameProject/src/model/GameBoard.java
@@ -8,5 +8,9 @@ public class GameBoard {
     private ArrayList<Character> tiles;
     public enum PremiumSquare {DOUBLE_LETTER_SCORE,TRIPLE_LETTER_SCORE, DOUBLE_WORD_SCORE, TRIPLE_WORD_SCORE}
     
+    public GameBoard() {
+    	this.grid = new char[15][15];
+    }
+    
     
 }
