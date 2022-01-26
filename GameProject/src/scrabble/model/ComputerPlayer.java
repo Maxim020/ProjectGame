2353dbaadc34fc926018c70932b4538eadfa7157
Maxim020 <m.frano@student.utwente.nl>
2022-01-26 11:48:37 +0100

    Computer player now prints its moves

--- GameProject/src/scrabble/model/ComputerPlayer.java
+++ GameProject/src/scrabble/model/ComputerPlayer.java
@@ -1,19 +1,10 @@
 package scrabble.model;
 
 import scrabble.model.letters.Bag;
-import scrabble.model.letters.LetterDeck;
-import scrabble.model.words.AdjacentWordChecker;
-import scrabble.model.words.InMemoryScrabbleWordChecker;
-import scrabble.model.words.ScrabbleWordChecker;
-import scrabble.model.words.WordScoreCounter;
 
 public class ComputerPlayer extends Player {
 	
-	private String name;
-	private int score;
-	private LetterDeck letterdeck;
 	private Strategy strategy;
-	private Board board;
 	
 	public ComputerPlayer(Bag bag, Strategy strategy, Board board) {
 		super("Smart Computer", bag);
@@ -26,11 +17,11 @@ public class ComputerPlayer extends Player {
 	}
 
 	public String determineMove(Board board) {
-		if(strategy.determineMove(board, this.getLetterDeck()) != "") {
-			return strategy.determineMove(board, this.getLetterDeck());
+		if(strategy.determineMove(board, getLetterDeck()) != "") {
+			return strategy.determineMove(board, getLetterDeck());
 		}
 		else {
-			return "SWAP " + strategy.swapHand(this.getLetterDeck());
+			return "SWAP " + strategy.swapHand(getLetterDeck());
 		}
 	}
 }
